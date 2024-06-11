a9f1103543b48e05ab73f23826fd13161499053c
nadia3190 <nadia.tlb.90@gmail.com>
2024-06-11 12:35:18 +0200

    adding backend side server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
